bbe7b6074fe939fda3eff5a7279ec1f4e846a672
KeithHagerich <khag3d@gmail.com>
2018-03-30 20:58:15 -0400

    deletions from base project addition of techan

---
+++ src/components/candlestickChart/CandleStick.js
@@ -0,0 +1,13 @@
+import React, {PropTypes} from 'react';
+
+const CandleStick = (props) => {
+    return (
+
+    );
+}
+
+CandleStick.propTypes = {
+    //myProp: PropTypes.string.isRequired
+};
+
+export default CandleStick;
